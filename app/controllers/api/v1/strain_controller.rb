cc67ca442dca9f068badfef7d2a82ca8ff3b4d65
James Lagattuta <jimmy.lagattuta@gmail.com>
2018-03-02 17:02:32 -0800

    update strains ready

--- app/controllers/api/v1/strain_controller.rb
+++ app/controllers/api/v1/strain_controller.rb
@@ -18,5 +18,31 @@ class Api::V1::StrainController < ApplicationController
 								)
 		redirect_to 'index.html.erb'
 	end
+
+	def edit_strain
+		strain = Strain.find_by(id: params[:id])
+		n = params['name']
+		v = params['variety']
+		donation_g = params['donation_g']
+		donation_e = params['donation_e']
+		donation_h = params['donation_h']
+		donation_o = params['donation_o']
+		i = params['strain_image']
+		strain.name = n
+		strain.variety = v
+		strain.donation_g = donation_g
+		strain.donation_e = donation_e	
+		strain.donation_h = donation_h	
+		strain.donation_o = donation_o
+		strain.image = i
+		strain.save
+		if strain.save
+			puts "*" * 100
+		else 
+			puts strain.errors.full_messages
+		end
+		redirect_to '/'
+
+	end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "api/v1/home#index"
 
-  post "/api/v1/add_strain" => "api/v1/strain#add_strain" 
+  post "/api/v1/add_strain" => "api/v1/strain#add_strain"
+  post "/api/v1/edit_strain/:id" => "api/v1/strain#edit_strain"
 end
